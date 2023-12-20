391f6e5421d126573b22b5ab3082d1a014d29922
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-12-20 12:22:33 +0530

    console error solved

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,10 +3,9 @@ import React, { useState, useEffect } from 'react';
 const Footer = () => {
     const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 
-    useEffect(() => {
-        document.getElementById("currentYear").textContent = currentYear;
-    }, [currentYear]);
-
+  useEffect(() => {
+    setCurrentYear(new Date().getFullYear());
+  }, []); 
     return (
         <div className='bg-medium_gray_two'>
             <div className='container xl:w-[1100px] px-3 sm:px-5 xl:px-0 mx-auto'>
@@ -15,7 +14,7 @@ const Footer = () => {
                     <li><button className='text-footer_light_gray font-Merriweather font-normal leading-[160%] text-xsm hover:text-black transition-all ease-linear duration-200'>Privacy Policy</button></li>
                     <li><button className='text-footer_light_gray font-Merriweather font-normal leading-[160%] text-xsm hover:text-black transition-all ease-linear duration-200'>Contact</button></li>
                 </ul>
-                <p className='text-center pb-9 pt-[26px] text-copyright_color text-xsm font-bold leading-[160%]'>Copyright © <span id="currentYear"></span> Food Ninja Blog. All Rights Reserved.</p>
+                <p className='text-center pb-9 pt-[26px] text-copyright_color text-xsm font-bold leading-[160%]'>Copyright © {currentYear} Food Ninja Blog. All Rights Reserved.</p>
             </div>
         </div>
     );
