3ffe5f15c04d0fd804578a2faa06b71bbe8c26f3
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-12-20 13:58:38 +0530

    aos

--- src/components/view/Homepage.jsx
+++ src/components/view/Homepage.jsx
@@ -1,17 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import MainPage from './MainPage'
 import SingleBlog from './SingleBlog'
 import { Route, Routes } from 'react-router-dom'
 import 'animate.css';
-import Aos from 'aos';
-import "aos/dist/aos.css";
 const Homepage = () => {
-    useEffect(() => {
-        Aos.init({
-            once: true,
-            duration: 1500,
-        });
-    }, []);
     return (
         <>
             <Routes>
